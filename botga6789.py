7c476b6bd69e9780e6a25c2869c4db22a20ac15a
lamlucas <168625798+lamlucas@users.noreply.github.com>
2025-07-05 14:46:09 +0700

    botga6789.py

--- botga6789.py
+++ botga6789.py
@@ -1,18 +1,18 @@
-import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import telebot
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        f'GA6789 xin chào {update.effective_user.first_name} \n'
-        '❤️ Nạp đầu tặng 100% (Đá gà, Casino, Thể Thao, Slot...)\n'
-        '❤️ Nạp đầu mỗi ngày tặng 10% (Thể thao, Đá gà)\n'
-        '❤️ Bảo hiểm 8% mỗi ngày (Đá gà)\n'
-        '💋 Link đăng ký: https://daga1111.com'
+TOKEN = "7961294831:AAGEOAbfx3KYYm9dCZ8yHE-QWf-IhK0jXd8"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    first_name = message.from_user.first_name
+    reply_text = (
+        f"GA6789 xin chào {first_name}!\n"
+        "❤️ Nạp đầu tặng 100% (Đá gà, Casino, Thể Thao, Slot...)\n"
+        "❤️ Nạp đầu mỗi ngày tặng 10% (Thể thao, Đá gà)\n"
+        "❤️ Bảo hiểm 8% mỗi ngày (Đá gà)\n"
+        "💋 Link đăng ký: https://daga1111.com"
     )
+    bot.reply_to(message, reply_text)
 
-TOKEN = os.environ["BOT_TOKEN"]
-
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
-app.run_polling()
+bot.infinity_polling()
